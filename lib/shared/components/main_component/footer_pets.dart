887598e0bc9bf5f8f81c7adf3e10cbba78df9e77
Ali Ashraf <56132081+ao307@users.noreply.github.com>
2022-07-27 23:54:20 +0200

    update

--- lib/shared/components/main_component/footer_pets.dart
+++ lib/shared/components/main_component/footer_pets.dart
@@ -7,7 +7,9 @@ import 'package:doctor_care/shared/themes/colors.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+//TODO: Footer OF PETS
 class FooterPets extends StatelessWidget {
   const FooterPets({Key? key}) : super(key: key);
 
@@ -38,19 +40,35 @@ class FooterPets extends StatelessWidget {
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        // first column
                         FooterItems(
                           topTitle: 'for any questions'.tr().toCapitalized(),
                           icon1: emailSvg,
-                          title1: 'email@gmail.com'.tr().toCapitalized(),
+                          title1: '${cubit(context).footerModel!.email}'
+                              .tr()
+                              .toCapitalized(),
+                          onTap1: () => launch(
+                            'mailto:${cubit(context).footerModel!.email}',
+                          ),
                           icon2: phoneSvg,
-                          title2: '+966 544 544 544'.tr().toCapitalized(),
+                          title2: '${cubit(context).footerModel!.phone}'
+                              .tr()
+                              .toCapitalized(),
+                          onTap2: () => launch(
+                            'tel:${cubit(context).footerModel!.phone}',
+                          ),
                         ),
+                        // second column
                         FooterItems(
                           topTitle: 'we are waiting you'.tr().toCapitalized(),
                           icon1: phoneSvg,
-                          title1: 'First settlement/Cairo'.tr().toCapitalized(),
+                          title1: '${cubit(context).footerModel!.location}'
+                              .tr()
+                              .toCapitalized(),
                           icon2: locationSvg,
-                          title2: 'First settlement/Cairo'.tr().toCapitalized(),
+                          title2: '${cubit(context).footerModel!.location2}'
+                              .tr()
+                              .toCapitalized(),
                         ),
                       ],
                     ),
@@ -80,7 +98,9 @@ class FooterItems extends StatelessWidget {
     required this.icon1,
     required this.icon2,
     required this.title1,
+    this.onTap1,
     required this.title2,
+    this.onTap2,
     required this.topTitle,
   }) : super(key: key);
   final String? topTitle;
@@ -88,6 +108,8 @@ class FooterItems extends StatelessWidget {
   final String? title1;
   final String? icon2;
   final String? title2;
+  final GestureTapCallback? onTap1;
+  final GestureTapCallback? onTap2;
 
   @override
   Widget build(BuildContext context) {
@@ -95,6 +117,7 @@ class FooterItems extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // top title
           Text(
             '$topTitle'.tr().toCapitalized(),
             style: const TextStyle(
@@ -106,30 +129,38 @@ class FooterItems extends StatelessWidget {
           const SizedBox(
             height: 25,
           ),
+          // first item
           Padding(
             padding: const EdgeInsets.only(bottom: 25.0),
-            child: ListTile(
-              leading: SVGString(icon1, width: 40),
-              title: Text(
-                '$title1'.tr().toCapitalized(),
-                style: const TextStyle(
-                  fontSize: 20.0,
-                  color: MyColors.cThirdColor,
-                  fontWeight: FontWeight.w700,
+            child: InkWell(
+              onTap: onTap1,
+              child: ListTile(
+                leading: SVGString(icon1, width: 40),
+                title: Text(
+                  '$title1'.tr().toCapitalized(),
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    color: MyColors.cThirdColor,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
             ),
           ),
+          // second item
           Padding(
             padding: const EdgeInsets.only(bottom: 25.0),
-            child: ListTile(
-              leading: SVGString(icon2, width: 40),
-              title: Text(
-                '$title2'.tr().toCapitalized(),
-                style: const TextStyle(
-                  fontSize: 20.0,
-                  color: MyColors.cThirdColor,
-                  fontWeight: FontWeight.w700,
+            child: InkWell(
+              onTap: onTap2,
+              child: ListTile(
+                leading: SVGString(icon2, width: 40),
+                title: Text(
+                  '$title2'.tr().toCapitalized(),
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    color: MyColors.cThirdColor,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
             ),
